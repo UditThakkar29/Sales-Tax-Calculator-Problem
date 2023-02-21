5320a6f3381d65b8b4f9ba65c4b296dacb786baa
Udit <uthakkar@gammastack.com>
2023-02-21 19:34:12 +0530

    Initial setup for tax

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative "tax"
+
+include Store
+
+
+def start
+  puts "Welcome to the shop"
+
+  get_all_products
+
+  input = 1
+  while input
+
+end
+
+start
---
+++ tax.rb
@@ -0,0 +1,18 @@
+$products = ["Books","Muisc CD","Chocolate Bar","Imported Perfume","Imported Chocolate Box", "Perfume","Headache Pills"]
+module Store
+
+  def get_all_products
+    i = 1
+    $products.each do |ele|
+      puts (i.to_s + " " + ele)
+      i+=1
+    end
+
+  end
+
+  class Products
+    def initialize()
+  end
+end
+
+
